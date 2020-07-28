6727f0e6e0d6adaabc59a57648e8050119cf30bb
Georgette Class-Peters <georgycp@udel.edu>
2020-07-28 16:19:19 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,18 +4,3 @@ end
 
 # call your method here!
 
-def hello_t(array)
-   i = 0
-   while i < array.length
-   i = i +1 
-  end
-end
-
-def hello_t(array)
-  i = 0
- 
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-end
