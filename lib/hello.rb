33561539f66dfbcc1e49a9617eb67050afda8396
Georgette Class-Peters <georgycp@udel.edu>
2020-07-28 16:14:34 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,3 +4,17 @@ end
 
 # call your method here!
 
+#hello_t calls the block once for each element in the passed-in array
+     Failure/Error:
+       def hello_t
+
+       end
+
+     
+  2) #hello_t returns the original array
+     Failure/Error:
+       def hello_t
+
+       end
+
+    
